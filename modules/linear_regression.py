aa9d379a60c08c16d1f685a5947d11445db77bbd
Blosuhm <zakhar.kruptsala@gmail.com>
2023-02-20 18:24:46 +0000

    something

--- modules/linear_regression.py
+++ modules/linear_regression.py
@@ -13,30 +13,13 @@ def linear_regression(array: list[tuple]):
     return slope, intercept
 
 
-# def plot_regression_line(array: list[tuple], slope: float, intercept: float):
-#     """Plots a linear regression line."""
-#     x = np.array([i[0] for i in array])
-#     y = np.array([i[1] for i in array])
-#     plt.scatter(x, y)
-#     plt.plot(x, slope * x + intercept, color="red")
-#     plt.show()
-
-
-def main():
-    """Main function."""
-    array = [
-        (222, 2.3),
-        (207.5, 2.2),
-        (194, 2),
-        (171.5, 1.8),
-        (153, 1.6),
-        (133, 1.4),
-        (113, 1.2),
-        (93, 1),
-    ]
-    slope, intercept = linear_regression(array)
-    print(slope, intercept)
-    # plot_regression_line(array, slope, intercept)
+def plot_regression_line(array: list[tuple], slope: float, intercept: float):
+    """Plots a linear regression line."""
+    x = np.array([i[0] for i in array])
+    y = np.array([i[1] for i in array])
+    plt.scatter(x, y)
+    plt.plot(x, slope * x + intercept, color="red")
+    plt.show()
 
 
 if __name__ == "__main__":
